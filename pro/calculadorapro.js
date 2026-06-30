3b185dee22f9a0f28992da0253a4480858c1935c
jorgeddcc7 <jorgeduenasc7@gmail.com>
2026-06-30 12:04:54 +0200

    popup5

--- pro/calculadorapro.js
+++ pro/calculadorapro.js
@@ -981,6 +981,6 @@ btnPopupCogton.addEventListener('click', () => {
         gtag('event', 'clic_solicitar_analisis');
     }
 
-    window.open('https://cogton.com/es/exporter-test', '_blank');
+    window.open('https://calendar.app.google/HAJCt6EPMxYdo9436', '_blank');
 
 });
